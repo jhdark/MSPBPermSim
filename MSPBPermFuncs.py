dc61142afc2536d7fead28531d0c23c6b482f5b3
WeaverC20 <67601091+WeaverC20@users.noreply.github.com>
2024-10-24 20:07:42 -0400

    working on convergence

--- MSPBPermFuncs.py
+++ MSPBPermFuncs.py
@@ -67,22 +67,24 @@ def ApplyBCs(my_model, T, P_up, barrier_solubility):
 
 def RunModel(my_model, final_time, folder):
     my_model.settings = F.Settings(
-        absolute_tolerance=1e-1,
-        relative_tolerance=1e-9,
+        absolute_tolerance=1e6,
+        relative_tolerance=1e-10,
         final_time=final_time  # s
         )
 
-    my_model.dt = F.Stepsize(initial_value=10,
+    my_model.dt = F.Stepsize(initial_value=100,
                             stepsize_change_ratio = 1.1)
 
     
     derived_quantities = F.DerivedQuantities([F.HydrogenFlux(surface=2)], show_units=True)
     txt_export = F.TXTExport(
         field='solute',
-        filename=folder + '/mobile.txt'
+        filename=folder + '/mobile.txt',
     )
 
-    my_model.exports = [derived_quantities, txt_export]
+    xdmf_export = F.XDMFExport(field="solute", folder=folder, checkpoint=False)
+
+    my_model.exports = [derived_quantities, txt_export, xdmf_export]
     my_model.initialise()
 
     my_model.run()
