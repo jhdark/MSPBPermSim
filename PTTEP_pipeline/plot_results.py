048fd6f68fb4605c5e43d4ae1c93db9e68a5b5af
jhdark <darkj385@mit.edu>
2025-03-24 13:23:27 -0400

    plot resutls

---
+++ PTTEP_pipeline/plot_results.py
@@ -0,0 +1,233 @@
+import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+from matplotlib import cm
+import numpy as np
+from scipy.integrate import cumulative_trapezoid
+from matplotlib import ticker
+
+# from labellines import labelLine, labelLines
+from pipe_losses import temperature_values, pressure_values
+
+
+def hydrogen_atoms_to_mass(atom_count: float) -> float:
+    """
+    Converts the number of hydrogen atoms to mass in grams.
+
+    Args:
+        atom_count: Number of hydrogen atoms.
+
+    returns:
+        float: Mass in grams.
+    """
+    atomic_mass_h = 1.00784 / (6.022e23)  # grams per hydrogen atom
+    return atom_count * atomic_mass_h
+
+
+def plot_standard():
+    data = np.genfromtxt("results/derived_quantities.csv", delimiter=",", names=True)
+
+    t = data["ts"]
+    flux = data["Flux_surface_2_0"] * -1
+
+    plt.figure()
+    plt.plot(t, flux)
+    plt.xlabel("Time (s)")
+    plt.ylabel("Surface Flux (m²/s)")
+
+
+def plot_item_1_1a():
+    ts = []
+    fluxes = []
+    for T in temperature_values:
+        for P in pressure_values:
+            data = np.genfromtxt(
+                f"results/item_1_1a/{T:.1f}K/{P:.2e}Pa/derived_quantities.csv",
+                delimiter=",",
+                names=True,
+            )
+            ts.append(data["ts"])
+            fluxes.append(data["Flux_surface_2_0"] * -1)
+
+    plt.figure()
+    plt.plot(ts, fluxes)
+    plt.xlabel("Time (s)")
+    plt.ylabel("Surface Flux (m²/s)")
+
+
+def plot_inventories_by_temperature():
+    ts = []
+    inventories = []
+    for T in temperature_values:
+        data = np.genfromtxt(
+            f"results/item_1_1a/{T:.1f}K/9.00+06Pa/derived_quantities.csv",
+            delimiter=",",
+            names=True,
+        )
+        ts.append(data["ts"])
+        inventories.append(data["Total_solute_volume_1_H_m2"])
+
+    pipe_wall_area = (np.pi * (323.9e-03 / 2) ** 2) - (
+        np.pi * ((329.9e-03 - 14.3e-03) / 2) ** 2
+    )
+    pipe_length = 2499  # m
+
+    inventories = np.array(inventories) * pipe_wall_area * pipe_length
+    inventories = hydrogen_atoms_to_mass(inventories)
+
+    norm_T = Normalize(vmin=min(pressure_values), vmax=max(pressure_values))
+    colorbar_T = cm.inferno
+    sm_T = plt.cm.ScalarMappable(cmap=colorbar_T, norm=norm_T)
+    colours_T = [colorbar_T(norm_T(i)) for i in pressure_values]
+
+    plt.figure()
+    for t, inv, c in zip(ts, inventories, colours_T):
+        plt.plot(t, inv, color=c)
+    plt.xlabel("Time (s)")
+    plt.ylabel("Hydrogen losses (g)")
+
+    ax = plt.gca()
+    plt.colorbar(sm_T, label=r"Temperature (K)", ax=ax, shrink=0.75)
+
+
+def plot_inventory_and_surface_flux(pressure_value: float, temperature_value: float):
+    data = np.genfromtxt(
+        f"results/item_1_1a/{temperature_value:.1f}K/{pressure_value:.2e}Pa/derived_quantities.csv",
+        delimiter=",",
+        names=True,
+    )
+    ts = data["ts"]
+    inventories = data["Total_solute_volume_1_H_m1"]
+    surface_fluxes = data["solute_flux_surface_2_H_m1_s1"] * -1
+
+    one_year = 3600 * 24 * 365
+    ts = np.array(ts) / one_year
+
+    pipe_length = 1000  # m
+
+    inventories = np.array(inventories) * pipe_length
+    inventories = hydrogen_atoms_to_mass(inventories)
+
+    surface_fluxes = np.array(surface_fluxes) * pipe_length  # to H/s/km
+    surface_fluxes = hydrogen_atoms_to_mass(surface_fluxes) * one_year  # to g/km/yr
+
+    plt.figure()
+    plt.plot(ts, inventories, color="black")
+    plt.xlabel("Time (yr)")
+    plt.ylabel("Mobile Hydrogen in wall (g/km)")
+    plt.ylim(bottom=0)
+    plt.xlim(0, 40)
+    ax = plt.gca()
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    plt.tight_layout()
+
+    plt.figure()
+    plt.fill_between(ts, surface_fluxes, color="black")
+    plt.xlabel("Time (yr)")
+    plt.ylabel("Instantaneous Hydrogen permeation losses to wall (g/km/yr)")
+    plt.xlim(0, 40)
+    plt.ylim(bottom=0)
+    ax = plt.gca()
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    plt.tight_layout()
+
+    plt.figure()
+    total_permeation_loss = cumulative_trapezoid(surface_fluxes, x=ts, initial=0)
+    plt.plot(ts, total_permeation_loss, color="black")
+    plt.xlabel("Time (yr)")
+    plt.ylabel("Cumulative hydrogen permeation losses to wall (g/km)")
+    plt.xlim(0, 40)
+    plt.ylim(bottom=0)
+    ax = plt.gca()
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    plt.tight_layout()
+
+
+def plot_mobile_conc_field_over_time(pressure_value: float, temperature_value: float):
+    data = np.genfromtxt(
+        f"results/item_1_1a/{temperature_value:.1f}K/{pressure_value:.2e}Pa/mobile_concentration_field.txt",
+        delimiter=",",
+        names=True,
+    )
+    # print(data.dtype.names)
+    # quit()
+    x = data["x"]
+    x = (x - x[0]) * 1000  # Convert meters to mm and moves left to zero
+
+    c_data = [
+        data["t315e07s"],
+        data["t158e08s"],
+        data["t315e08s"],
+        data["t631e08s"],
+        data["t126e09s"],
+    ]
+    c_data_g = []
+    for data in c_data:
+        c_data_g.append(hydrogen_atoms_to_mass(data))
+
+    # Function to label the lines inline
+    def label_lines(lines, xvals, colours):
+        for line, xval, colour in zip(lines, xvals, colours):
+            xdata, ydata = line.get_xdata(), line.get_ydata()
+            yval = np.interp(xval, xdata, ydata)  # Interpolate y-value at xval
+            label = line.get_label()
+
+            plt.annotate(
+                label,
+                xy=(xval, yval),
+                xytext=(0, 0),
+                textcoords="offset points",
+                fontsize=12,
+                va="center",
+                ha="center",
+                bbox=dict(facecolor="white", edgecolor="none"),
+                color=colour,
+            )
+
+    # Custom x-positions for labels (closer to the start)
+    label_positions = [
+        x[500],
+        x[1300],
+        x[2000],
+        x[3000],
+        x[4500],
+    ]  # Adjust these indices as needed
+
+    year_values = [1, 5, 10, 20, 40]
+
+    norm = Normalize(vmin=min(year_values), vmax=max(year_values))
+    colorbar = cm.viridis
+    colours = [colorbar(norm(i)) for i in year_values]
+
+    plt.figure()
+    lines = []
+    for c, label, colour in zip(c_data_g, ["1y", "5y", "10y", "20y", "40y"], colours):
+        (line,) = plt.plot(x, c, label=label, color=colour)
+        lines.append(line)
+    plt.xlim(x[0], x[-1])
+    plt.ylim(bottom=0)
+    plt.xlabel("Pipe wall (mm)")
+    plt.ylabel("Mobile H Concentration (g/m)")
+    ax = plt.gca()
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    label_lines(lines, label_positions, colours)
+    plt.tight_layout()
+
+
+if __name__ == "__main__":
+    # plot_standard()
+    # plot_item_1_1a()
+    # plot_inventories_by_pressure(pressure_value=pressure_values[-1])
+    # plot_inventories_by_temperature()
+
+    plot_inventory_and_surface_flux(
+        pressure_value=pressure_values[-1], temperature_value=temperature_values[-1]
+    )
+    plot_mobile_conc_field_over_time(
+        pressure_value=pressure_values[-1], temperature_value=temperature_values[-1]
+    )
+
+    plt.show()
